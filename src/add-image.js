1c04ed07992426526b3e9f344a17464c6ec50f0b
Logan Thomas <logan.thomas@forsta.com>
2022-07-04 17:28:00 -0700

    installed and added loaders to webpack

---
+++ src/add-image.js
@@ -0,0 +1,11 @@
+import Kiwi from "./kiwi.jpg";
+import altText from "./altText.txt";
+
+export const addImage = () => {
+  const img = document.createElement("img");
+  img.alt = altText;
+  img.width = 300;
+  img.src = Kiwi;
+  const body = document.querySelector("body");
+  body.appendChild(img);
+};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3 kilobytes
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
